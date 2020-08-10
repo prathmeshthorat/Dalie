731b91a6da5eb4b2df34c3211ae78ef846705453
prathmeshthorat <prathmeshthorat06@gmail.com>
2020-08-11 00:23:57 +0530

    Restructuring project to recognise as mvn proj

---
+++ src/main/java/com/dalie/Dalie/DalieApplication.java
@@ -0,0 +1,13 @@
+package com.dalie.Dalie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dalie/Dalie/DalieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dalie.Dalie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
