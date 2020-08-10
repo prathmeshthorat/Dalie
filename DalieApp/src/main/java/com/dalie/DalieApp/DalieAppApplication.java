44bdac1f4651d0c65894d5decd0571acca6414a3
prathmeshthorat <58651263+prathmeshthorat@users.noreply.github.com>
2020-08-10 23:15:40 +0530

    Adding project structure

---
+++ DalieApp/src/main/java/com/dalie/DalieApp/DalieAppApplication.java
@@ -0,0 +1,13 @@
+package com.dalie.DalieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalieAppApplication.class, args);
+	}
+
+}
